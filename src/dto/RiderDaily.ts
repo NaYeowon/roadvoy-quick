c0723f8315b76ce4029492f9155fb5dbd70bae7e
so <socomee@naver.com>
2021-08-02 08:21:01 +0900

    DTO 정의 추가

---
+++ src/dto/RiderDaily.ts
@@ -0,0 +1,25 @@
+export default interface RiderDaily {
+  ucAreaNo: number;
+
+  ucDistribId: number;
+
+  ucAgencyId: number;
+
+  ucMemCourId: number;
+
+  acBizDate: string;
+
+  usDayDoneCallSum: number;
+
+  lDayDeliCost: number;
+
+  lDayTotalRevenue: number;
+
+  ulDayDeliChargeTotal: number;
+
+  lDayCycleLeaseCost: number;
+
+  lRepayCycleLeaseCost: number;
+
+  lRepayPrevNonCycleLeaseCost: number;
+}
---
+++ src/dto/RiderDailyTotal.ts
@@ -0,0 +1,33 @@
+export default interface RiderDailyTotal {
+  usDayDoneCallSum: number;
+
+  lDayTotalRevenue: number;
+
+  lDayDeliCost: number;
+
+  lDayCycleLeaseCost: number;
+
+  ulSubstituteCashPlus: number;
+
+  ulSubstituteCashMinus: number;
+
+  ulSubstituteInput: number;
+
+  ulSubstituteRefund: number;
+
+  ulSubstituteDeposit: number;
+
+  ulVirBankDeposit: number;
+
+  ulReClaimAmount: number;
+
+  ulReClaimComm: number;
+
+  ulCreditAmount: number;
+
+  usDayDoneErrandSum: number;
+
+  lDayErrandCharge: number;
+
+  lDayErrandFeeAgency: number;
+}
