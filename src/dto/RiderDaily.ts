acdd5e4a54f73ba9bb7f9496d44fb25ed8b8a802
NaYeowon <77387758+NaYeowon@users.noreply.github.com>
2021-08-02 18:53:30 +0900

    DTO정의 추가

---
+++ src/dto/RiderDaily.ts
@@ -0,0 +1,26 @@
+/* eslint-disable */
+export default interface RiderDaily {
+  ucAreaNo: number;
+
+  ucDistribId: number;
+
+  ucAgencyId: number;
+
+  ucMemCourId: number;
+
+  acBizDate: string;
+
+  usDayDoneCallSum: number;
+
+  lDayDeliCost: number;
+
+  lDayTotalRevenue: number;
+
+  ulDayDeliChargeTotal: number;
+
+  lDayCycleLeaseCost: number;
+
+  lRepayCycleLeaseCost: number;
+
+  lRepayPrevNonCycleLeaseCost: number;
+}
