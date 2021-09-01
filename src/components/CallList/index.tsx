ef41cb862167596efe9279c6f490df0dfe9055b1
NaYeowon <duwon716@daum.net>
2021-09-01 18:20:32 +0900

    interface 수정

--- src/components/CallList/index.tsx
+++ src/components/CallList/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
-import { Col, Row } from "antd";
+import { Col, Row, Tag } from "antd";
 import styled from "styled-components";
+import { CallInfo } from "./CallListComponent";
 
 const Title = styled.span`
   color: #000;
@@ -9,7 +10,7 @@ const Title = styled.span`
 
 interface Props {
   title: string;
-  value: string;
+  value: any;
 }
 
 export const CallDetailShopTitle = (props: Props) => {
@@ -22,3 +23,39 @@ export const CallDetailShopTitle = (props: Props) => {
     </Row>
   );
 };
+
+interface Call {
+  callInfo: CallInfo;
+}
+
+// export const PaymentModeAndAmount = (props: Call) => {
+//   const { callInfo } = props;
+
+//   // eslint-disable-next-line radix
+//   const ucPaymentMode: number = callInfo.ucPaymentMode;
+//   const charge = Number(callInfo.ulGoodsPrice).toLocaleString();
+
+//   let PaymentModeText;
+//   let paymentColor;
+
+//   switch (ucPaymentMode) {
+//     case 2:
+//       PaymentModeText = "카드";
+//       paymentColor = "#2db7f5";
+//       break;
+//     case 3:
+//       PaymentModeText = "현금";
+//       paymentColor = "#87d068";
+//       break;
+//     case 4:
+//       PaymentModeText = "선결";
+//       paymentColor = "#f50";
+//       break;
+//   }
+//   return (
+//     <>
+//       <Tag color={paymentColor}>{PaymentModeText}</Tag>
+//       <span>{charge}원</span>
+//     </>
+//   );
+// };
