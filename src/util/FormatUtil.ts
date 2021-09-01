ca7223633d0db57d29ce73eade0cd1e33ee5678e
NaYeowon <duwon716@daum.net>
2021-09-01 18:03:10 +0900

    전화번호 형식

--- src/util/FormatUtil.ts
+++ src/util/FormatUtil.ts
@@ -5,12 +5,21 @@ export const getDateFormat = (date: string) => {
   return dateFormat[1];
 };
 
-export const getCellNoFormat = (phone: string) => {
-  const one = phone.substring(0, 3);
-  const two = phone.substring(3, 7);
-  const three = phone.substring(7, 11);
-  return `${one}-${two}-${three}`;
+export const getCellNoFormat = phone => {
+  if (!phone || phone.length === 0) {
+    return null;
+  }
+  return phone
+    .replace(/[^0-9]/g, "")
+    .replace(/(^02|^0504|^0508|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})/, "$1-$2-$3")
+    .replace("--", "-");
 };
+// export const getCellNoFormat = (phone: string) => {
+//   const one = phone.substring(0, 3);
+//   const two = phone.substring(3, 7);
+//   const three = phone.substring(7, 11);
+//   return `${one}-${two}-${three}`;
+// };
 
 export const costFormat = (cost: number) => {
   const format = Number(cost);
