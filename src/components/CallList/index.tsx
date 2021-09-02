13b4e4badca989a3fc82015d7308f6198a031d1e
나여원 <ywna@roadvoy.com>
2021-09-02 19:47:57 +0900

    주석 삭제

--- src/components/CallList/index.tsx
+++ src/components/CallList/index.tsx
@@ -23,39 +23,3 @@ export const CallDetailShopTitle = (props: Props) => {
     </Row>
   );
 };
-
-interface Call {
-  callInfo: CallInfo;
-}
-
-// export const PaymentModeAndAmount = (props: Call) => {
-//   const { callInfo } = props;
-
-//   // eslint-disable-next-line radix
-//   const ucPaymentMode: number = callInfo.ucPaymentMode;
-//   const charge = Number(callInfo.ulGoodsPrice).toLocaleString();
-
-//   let PaymentModeText;
-//   let paymentColor;
-
-//   switch (ucPaymentMode) {
-//     case 2:
-//       PaymentModeText = "카드";
-//       paymentColor = "#2db7f5";
-//       break;
-//     case 3:
-//       PaymentModeText = "현금";
-//       paymentColor = "#87d068";
-//       break;
-//     case 4:
-//       PaymentModeText = "선결";
-//       paymentColor = "#f50";
-//       break;
-//   }
-//   return (
-//     <>
-//       <Tag color={paymentColor}>{PaymentModeText}</Tag>
-//       <span>{charge}원</span>
-//     </>
-//   );
-// };
