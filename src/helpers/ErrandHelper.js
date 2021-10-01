82351679f7873ea5f41a26bb3210a271652d8e58
나여원 <duwon716@daum.net>
2021-10-01 15:42:41 +0900

    경유지 Tag추가

--- src/helpers/ErrandHelper.js
+++ src/helpers/ErrandHelper.js
@@ -1,24 +1,49 @@
-/* eslint-disable */
-import React from 'react'
-import { Tag } from 'antd'
+import React from "react";
+import { Tag } from "antd";
 
 class ErrandHelper {
-  static formatAddress (stErrand) {
+  static formatAddress(stErrand) {
     if (parseInt(stErrand.ucErrandType) === 1) {
-      return <div>
+      return (
         <div>
-          <Tag color="volcano">픽업지</Tag>{stErrand.acOriginOldAddr} {stErrand.acOriginAddrDesc}
+          <div>
+            <Tag color="volcano">픽업지</Tag>
+            {stErrand.acOriginOldAddr} {stErrand.acOriginAddrDesc}
+          </div>
+          <div>
+            <Tag color="purple">목적지</Tag>
+            {stErrand.acDestOldAddr}
+            {stErrand.acDestAddrDesc}
+          </div>
         </div>
+      );
+    } else if (parseInt(stErrand.ucErrandType) === 2) {
+      return (
         <div>
-          <Tag color="purple">목적지</Tag>{stErrand.acDestOldAddr}{stErrand.acDestAddrDesc}
+          <Tag color="purple">목적지</Tag>
+          {stErrand.acDestOldAddr} {stErrand.acDestOldAddr}
         </div>
-      </div>
-    } else {
-      return <div>
-        <Tag color="purple">목적지</Tag>{stErrand.acDestOldAddr} {stErrand.acDestOldAddr}
-      </div>
+      );
+    } else if (parseInt(stErrand.ucErrandType) === 3) {
+      return (
+        <div>
+          <div>
+            <Tag color="volcano">픽업지</Tag>
+            {stErrand.acOriginOldAddr} {stErrand.acOriginAddrDesc}
+          </div>
+          <div>
+            <Tag color="purple">목적지</Tag>
+            {stErrand.acDestOldAddr}
+            {stErrand.acDestAddrDesc}
+          </div>
+          <div>
+            <Tag color="purple">경유지1</Tag>
+            {stErrand.acStop1OldAddr} {stErrand.acStop1OldAddr}
+          </div>
+        </div>
+      );
     }
   }
 }
 
-export default ErrandHelper
+export default ErrandHelper;
