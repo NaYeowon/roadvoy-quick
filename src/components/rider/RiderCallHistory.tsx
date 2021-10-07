dcd25f54904dd030f2f2685ad9f85eb3a3d0737c
나여원 <duwon716@daum.net>
2021-10-07 18:07:28 +0900

    기사별 콜 이력 화면

--- src/components/rider/RiderCallHistory.tsx
+++ src/components/rider/RiderCallHistory.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable */
 
+import { Table } from "antd";
 import React, { Component } from "react";
 import { CallInfo } from "../CallList/CallListComponent";
 import Header from "../Layout/Header";
@@ -12,11 +13,66 @@ interface IState {
   astCall: CallInfo[];
 }
 
+const columns = [
+  {
+    title: "가맹명",
+    dataIndex: "",
+    key: "",
+    width: 100,
+  },
+  {
+    title: "점수",
+    dataIndex: "",
+    key: "",
+    width: 50,
+  },
+  {
+    title: "진행/조리",
+    dataIndex: "",
+    key: "",
+    width: 50,
+  },
+  {
+    title: "주소",
+    dataIndex: "",
+    key: "",
+    width: 200,
+  },
+  {
+    title: "배달비",
+    dataIndex: "",
+    key: "",
+    width: 80,
+  },
+  {
+    title: "결제정보",
+    dataIndex: "",
+    key: "",
+    width: 80,
+  },
+  {
+    title: "기사",
+    dataIndex: "",
+    key: "",
+    width: 80,
+  },
+  {
+    title: "고객연락처",
+    dataIndex: "",
+    key: "",
+    width: 80,
+  },
+];
 class RiderCallHistory extends Component<IProps, IState> {
   render() {
     return (
       <>
         <Header />
+        <Table
+          columns={columns}
+          bordered
+          style={{ width: "100%", height: "100%", cursor: "pointer" }}
+        />
       </>
     );
   }
