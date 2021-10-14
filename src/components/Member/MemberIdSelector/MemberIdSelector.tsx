4a37bcc6b9b6486f6b8705774ae8b0a596960619
나여원 <duwon716@daum.net>
2021-10-14 14:09:41 +0900

    선택변경시 selectBox 초기화

--- src/components/Member/MemberIdSelector/MemberIdSelector.tsx
+++ src/components/Member/MemberIdSelector/MemberIdSelector.tsx
@@ -1,4 +1,5 @@
 import { Select, Spin } from "antd";
+import { useEffect, useState } from "react";
 import { MemberIdDto } from "../../../domain/Member/model";
 
 const { Option } = Select;
@@ -11,12 +12,21 @@ interface MemberIdProps {
 export default function MemberIdSelector(props: MemberIdProps) {
   const { memberIds, onChange } = props;
 
+  const [select, setSelect] = useState<string>("선택하세요");
+
+  useEffect(() => {
+    setSelect("선택하세요");
+  }, [memberIds]);
+
   return (
     <Select
       defaultValue={"선택하세요"}
       onChange={(value: string) => {
-        onChange(JSON.parse(value));
+        const it = JSON.parse(value);
+        setSelect(it.acCompany || it.acPresident || it.ucAreaNo);
+        onChange(it);
       }}
+      value={select}
     >
       {memberIds.map(it => (
         <Option key={JSON.stringify(it)} value={JSON.stringify(it)}>
