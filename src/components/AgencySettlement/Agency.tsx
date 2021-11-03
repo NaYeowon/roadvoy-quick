a2d38ede4b4046851f9cfeb9c481956a6d6f7b17
나여원 <duwon716@daum.net>
2021-11-03 12:30:37 +0900

    columns값, api 수정

--- src/components/AgencySettlement/Agency.tsx
+++ src/components/AgencySettlement/Agency.tsx
@@ -6,14 +6,14 @@ import Header from "../Layout/Header";
 
 import { message, PageHeader, Table } from "antd";
 import "antd/dist/antd.css";
-import axios from "axios";
 
 import LoginHelper from "../../pages/shared/LoginHelper";
 import { AgencyDTO } from "../shop/types";
 import api from "src/config/axios";
 import { MemberGroupSelector } from "../Member";
 import AgencyDetail from "./AgencyDetail";
-
+import MemberHelper from "src/helpers/MemberHelper";
+import { DateFormat, getCellNoFormat } from "src/util/FormatUtil";
 const columns = [
   {
     title: "계정정보",
@@ -21,7 +21,7 @@ const columns = [
       {
         title: "아이디",
         dataIndex: "ucMemCourId",
-        key: "ucMemCourId",
+        render: (text: string, record: AgencyDTO) => `${MemberHelper.formatMemberId(record)}`,
         width: 70,
       },
       {
@@ -42,11 +42,21 @@ const columns = [
       {
         title: "가입일자",
         dataIndex: "acEntryDateTime",
+        render: (data: string) => DateFormat(data),
+
         width: 50,
       },
       {
         title: "세금계산서발행",
         dataIndex: "ucTaxInvoType",
+        render: (value: number, record: AgencyDTO) => {
+          switch (Number(value)) {
+            case 0:
+              return "미발행";
+            case 1:
+              return "발행";
+          }
+        },
         width: 80,
       },
       {
@@ -62,17 +72,22 @@ const columns = [
       {
         title: "(업체)전화번호",
         dataIndex: "acPhoneNo",
+        render: (phone: string) => getCellNoFormat(phone),
         width: 80,
       },
       {
         title: "(휴대)전화번호",
         dataIndex: "acCellNo",
+        render: (phone: string) => getCellNoFormat(phone),
         width: 80,
       },
       {
         title: "주소",
         dataIndex: "acOldAddress" + "acAddressDesc",
-        width: 100,
+        render: (text: string, record: AgencyDTO) => {
+          return `${record.acOldAddress} ${record.acAddressDesc}`;
+        },
+        width: 200,
       },
     ],
   },
@@ -97,6 +112,46 @@ const columns = [
       {
         title: "거래은행",
         dataIndex: "usVirtualBank",
+        render: (value: number, record: AgencyDTO) => {
+          switch (Number(value)) {
+            case 88:
+              return "신한은행";
+            case 4:
+              return "국민은행";
+            case 3:
+              return "기업은행";
+            case 20:
+              return "우리은행";
+            case 90:
+              return "카카오뱅크";
+            case 89:
+              return "케이뱅크";
+            case 11:
+              return "농협중앙회";
+            case 2:
+              return "산업은행";
+            case 23:
+              return "SC제일은행";
+            case 81:
+              return "KEB하나은행";
+            case 27:
+              return "씨티뱅크";
+            case 7:
+              return "수협은행";
+            case 31:
+              return "대구은행";
+            case 32:
+              return "부산은행";
+            case 34:
+              return "광주은행";
+            case 35:
+              return "제주은행";
+            case 37:
+              return "전북은행";
+            case 39:
+              return "경남은행";
+          }
+        },
         width: 50,
       },
       {
@@ -121,13 +176,13 @@ const Agency = () => {
     try {
       const response = await api({
         method: "get",
-        url: "/agency/errand/list.php",
+        url: "/hq/member/process-query/get-agencies.php",
         headers: {
           Authorization: `Bearer ${LoginHelper.getToken()}`,
         },
       });
 
-      setAstManagerAgency(response.data.astManagerAgency);
+      setAstManagerAgency(response.data.lstMember);
     } catch (e) {
       const error = e as Error;
       message.error(error.message);
@@ -152,6 +207,7 @@ const Agency = () => {
       </PageHeader>
       <Table
         columns={columns}
+        dataSource={astManagerAgency}
         bordered
         pagination={false}
         size="small"
