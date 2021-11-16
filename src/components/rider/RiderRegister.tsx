b9ead82230bbf1ee9bce7f0b25d35073bd92cbfe
나여원 <duwon716@daum.net>
2021-11-16 13:13:12 +0900

    select option value 변경

--- src/components/rider/RiderRegister.tsx
+++ src/components/rider/RiderRegister.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import * as React from "react";
 import { useEffect, useMemo, useState } from "react";
-import { Input, Row, Col, Select, Button, message, Form, Popconfirm, Result } from "antd";
+import { Input, Row, Col, Select, Button, message, Form, Popconfirm } from "antd";
 import { PhoneOutlined } from "@ant-design/icons";
 import { AxiosError } from "axios";
 import "./RiderSettlementList.css";
@@ -368,51 +368,31 @@ const RiderRegister = (props: Props) => {
               <Form.Item label="주거래은행">
                 <Select
                   value={form.usBankCode}
-                  onChange={e => setForm({ ...form, usBankCode: Number(e) })}
+                  onChange={e =>
+                    setForm({
+                      ...form,
+                      usBankCode: Number(e),
+                    })
+                  }
                 >
-                  {isUpdate() ? (
-                    <>
-                      <Option value="88">신한은행</Option>
-                      <Option value="4">국민은행</Option>
-                      <Option value="3">기업은행</Option>
-                      <Option value="20">우리은행</Option>
-                      <Option value="90">카카오뱅크</Option>
-                      <Option value="89">케이뱅크</Option>
-                      <Option value="11">농협중앙회</Option>
-                      <Option value="2">산업은행</Option>
-                      <Option value="23">SC제일은행</Option>
-                      <Option value="81">KEB하나은행</Option>
-                      <Option value="27">씨티뱅크</Option>
-                      <Option value="7">수협은행</Option>
-                      <Option value="31">대구은행</Option>
-                      <Option value="32">부산은행</Option>
-                      <Option value="34">광주은행</Option>
-                      <Option value="35">제주은행</Option>
-                      <Option value="37">전북은행</Option>
-                      <Option value="39">경남은행</Option>
-                    </>
-                  ) : (
-                    <>
-                      <Option value={88}>신한은행</Option>
-                      <Option value={4}>국민은행</Option>
-                      <Option value={3}>기업은행</Option>
-                      <Option value={20}>우리은행</Option>
-                      <Option value={90}>카카오뱅크</Option>
-                      <Option value={89}>케이뱅크</Option>
-                      <Option value={11}>농협중앙회</Option>
-                      <Option value={2}>산업은행</Option>
-                      <Option value={23}>SC제일은행</Option>
-                      <Option value={81}>KEB하나은행</Option>
-                      <Option value={27}>씨티뱅크</Option>
-                      <Option value={7}>수협은행</Option>
-                      <Option value={31}>대구은행</Option>
-                      <Option value={32}>부산은행</Option>
-                      <Option value={34}>광주은행</Option>
-                      <Option value={35}>제주은행</Option>
-                      <Option value={37}>전북은행</Option>
-                      <Option value={39}>경남은행</Option>
-                    </>
-                  )}
+                  <Option value={88}>신한은행</Option>
+                  <Option value={4}>국민은행</Option>
+                  <Option value={3}>기업은행</Option>
+                  <Option value={20}>우리은행</Option>
+                  <Option value={90}>카카오뱅크</Option>
+                  <Option value={89}>케이뱅크</Option>
+                  <Option value={11}>농협중앙회</Option>
+                  <Option value={2}>산업은행</Option>
+                  <Option value={23}>SC제일은행</Option>
+                  <Option value={81}>KEB하나은행</Option>
+                  <Option value={27}>씨티뱅크</Option>
+                  <Option value={7}>수협은행</Option>
+                  <Option value={31}>대구은행</Option>
+                  <Option value={32}>부산은행</Option>
+                  <Option value={34}>광주은행</Option>
+                  <Option value={35}>제주은행</Option>
+                  <Option value={37}>전북은행</Option>
+                  <Option value={39}>경남은행</Option>
                 </Select>
               </Form.Item>
               <Form.Item label="주거래 계좌번호">
@@ -468,17 +448,8 @@ const RiderRegister = (props: Props) => {
                   value={form.ucCourierTag}
                   onChange={e => setForm({ ...form, ucCourierTag: Number(e) })}
                 >
-                  {isUpdate() ? (
-                    <>
-                      <Option value="1">지입</Option>
-                      <Option value="2">리스</Option>
-                    </>
-                  ) : (
-                    <>
-                      <Option value={1}>지입</Option>
-                      <Option value={2}>리스</Option>
-                    </>
-                  )}
+                  <Option value={1}>지입</Option>
+                  <Option value={2}>리스</Option>
                 </Select>
               </Form.Item>
               <Form.Item label="1일 리스료">
