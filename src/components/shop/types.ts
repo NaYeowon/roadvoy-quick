78b821546328be17f32c06539f8770df508f4632
나여원 <duwon716@daum.net>
2021-11-18 11:34:56 +0900

    CallQuantityDto 추가

--- src/components/shop/types.ts
+++ src/components/shop/types.ts
@@ -322,6 +322,23 @@ export interface DistributorDto {
   acCpCellNo: string;
   acWithdrawPassword: string;
 }
+
+export interface CallQuantityDto {
+  acCompany: string;
+  ulYearTotalCallCount: number;
+  ulMonth1CallCount: number;
+  ulMonth2CallCount: number;
+  ulMonth3CallCount: number;
+  ulMonth4CallCount: number;
+  ulMonth5CallCount: number;
+  ulMonth6CallCount: number;
+  ulMonth7CallCount: number;
+  ulMonth8CallCount: number;
+  ulMonth9CallCount: number;
+  ulMonth10CallCount: number;
+  ulMonth11CallCount: number;
+  ulMonth12CallCount: number;
+}
 class RiderInfo1 {
   asStatusMessage: string | undefined;
 
